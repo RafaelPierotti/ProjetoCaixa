f3ac1d9e5f478fba458528a520dfa0184a384f71
RafaelPierotti <rafaelpierotti236@gmail.com>
2025-01-02 17:58:21 -0300

    Projeot inicial

---
+++ src/main/java/br/com/projeto/caixa/ViewController.java
@@ -0,0 +1,13 @@
+package br.com.projeto.caixa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import net.rgielen.fxweaver.core.FxmlView;
+
+@Component
+@FxmlView("view.fxml")
+public class ViewController {
+
+    @Autowired
+
+}
